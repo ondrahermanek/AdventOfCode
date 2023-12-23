22e8144dfa0df8c90af67592eba07c28839c3eb5
Ondra Heřmánek <ondra.hermanek@gmail.com>
2023-12-23 13:05:20 +0100

    Task5 - part 2 - super INEFFECTIVE

--- AdventOfCode/AoC2023/Task05.cs
+++ AdventOfCode/AoC2023/Task05.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Diagnostics;
 using System.Linq;
 using System.Reflection;
 using System.Runtime.ExceptionServices;
@@ -16,17 +17,25 @@ namespace AoC2023
         public async Task<List<string>> Run()
         {
             var lines = await Utilities.ReadFileAsync("Input5.txt");
-            var seeds = GetSeeds(lines[0]);
+            var seeds = GetSeeds(lines[0]).ToArray();
             CheckBlankLine(lines[1]);
 
             var mapsByOrder = GetMaps(lines.Skip(2)).GroupBy(m => m.Order).ToDictionary(g => g.Key, g => g.ToArray()); // TODO: order by source something?
 
-            return seeds.Select(s => FindDestination(s, mapsByOrder)).Min().ToString().ToEnumerable().AsList();
+            return seeds.SelectMany(s => FindDestination(s, mapsByOrder)).Min().ToString().ToEnumerable().AsList();
         }
 
-        private Seed[] GetSeeds(string line)
+        private IEnumerable<Seed> GetSeeds(string line)
         {
-            return Utilities.ToLongs(line.Replace("seeds:", "")).Select(i => new Seed(i)).ToArray();
+            var values = Utilities.ToLongs(line.Replace("seeds:", ""));
+            if (values.Length % 2 == 1) throw new InvalidOperationException("Expected even count of seed values.");
+
+            while (values.NonEmpty())
+            {
+                yield return new Seed(values[0], values[1]);
+
+                values = values.Skip(2).ToArray();
+            }
         }
 
         private IEnumerable<Map> GetMaps(IEnumerable<string> lines)
@@ -76,19 +85,27 @@ namespace AoC2023
             }
         }
 
-        private long FindDestination(Seed seed, Dictionary<int, Map[]> mapsByOrder)
+        private IEnumerable<long> FindDestination(Seed seed, Dictionary<int, Map[]> mapsByOrder)
         {
-            var soil = FindDestination(seed.Number, mapsByOrder[0]);
-            var fertilizer = FindDestination(soil, mapsByOrder[1]);
-            var water = FindDestination(fertilizer, mapsByOrder[2]);
-            var light = FindDestination(water, mapsByOrder[3]);
-            var temp = FindDestination(light, mapsByOrder[4]);
-            var humidity = FindDestination(temp, mapsByOrder[5]);
-            var location = FindDestination(humidity, mapsByOrder[6]);
+            var stopWatch = new Stopwatch();
+            stopWatch.Start();
+            for (long i = 0; i < seed.Count; ++i)
+            {
+                var seedNumber = seed.Start + i;
+                var soil = FindDestination(seedNumber, mapsByOrder[0]);
+                var fertilizer = FindDestination(soil, mapsByOrder[1]);
+                var water = FindDestination(fertilizer, mapsByOrder[2]);
+                var light = FindDestination(water, mapsByOrder[3]);
+                var temp = FindDestination(light, mapsByOrder[4]);
+                var humidity = FindDestination(temp, mapsByOrder[5]);
+                var location = FindDestination(humidity, mapsByOrder[6]);
 
-            Console.WriteLine($"Seed {seed.Number} -> Soil {soil} -> Fertilizer {fertilizer} -> Water {water} -> Light {light} -> Temp {temp} -> Hum {humidity} -> Loc {location}.");
+                //Console.WriteLine($"Seed {seedNumber} -> Soil {soil} -> Fertilizer {fertilizer} -> Water {water} -> Light {light} -> Temp {temp} -> Hum {humidity} -> Loc {location}.");
 
-            return location;
+                yield return location;
+            }
+
+            Console.WriteLine($"Seed {seed.Start} computed in {stopWatch.Elapsed.TotalSeconds} s.");
         }
 
         private long FindDestination(long source, Map[] maps)
@@ -113,7 +130,7 @@ namespace AoC2023
             if (line.NonEmpty()) throw new InvalidOperationException("Blank line expected.");
         }
 
-        private record Seed(long Number);
+        private record Seed(long Start, long Count);
 
         private record Map(string Name, int Order, long DestinationStart, long DestinationEnd, long SourceStart, long SourceEnd, long Lenght)
         {
